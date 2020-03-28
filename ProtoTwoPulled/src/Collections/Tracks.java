41f7432901c8b9a0f060700d27b4d23f5eab2541
Scott Underwood <s-underwood@wiu.edu>
2020-03-28 15:40:44 -0500

    tutorial screen update

---
+++ ProtoTwoPulled/src/Collections/Tracks.java
@@ -0,0 +1,39 @@
+/*
+ * this product is an unlicensed as I am to drive
+ * 
+ * but hey, enjoy it anyway!
+ */
+package Collections;
+
+/**
+ *
+ * @author Scott
+ */
+public enum Tracks {
+       
+    TRACK_ZERO(0, "Start Theme", "/music/TitleScreen.wav");
+    
+    private final int number;
+    private final String description;
+    private final String filePath;
+    
+    private Tracks(int number, String description, String filePath){
+        
+        this.number = number;
+        this.description = description;
+        this.filePath = filePath;
+    }
+    
+    public int getNumber(){
+        return this.number;
+    }
+    
+    public String getDescription() {
+        return this.description;
+    }
+
+    public String getFilePath() {
+        return this.filePath;
+    }
+    
+}
