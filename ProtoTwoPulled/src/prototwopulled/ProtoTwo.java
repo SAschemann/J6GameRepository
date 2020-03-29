3800bb3bb94468af15d18aa57bba360d5efd75b0
Scott Underwood <s-underwood@wiu.edu>
2020-03-28 23:13:36 -0500

    save file created, needs implemented to GUI

--- ProtoTwoPulled/src/prototwopulled/ProtoTwo.java
+++ ProtoTwoPulled/src/prototwopulled/ProtoTwo.java
@@ -2,17 +2,17 @@
 This is the class which contains the main method. This creates the main menu and sets it to visible.
  */
 package prototwopulled;
-
+import javax.sound.sampled.LineUnavailableException;
+import javax.sound.sampled.UnsupportedAudioFileException;
 /**
  *
  * @author asche
  */
 public class ProtoTwo {
 
-    /**
-     * @param args the command line arguments
-     */
+    public static UserPlayer CUR_PLAYER = new UserPlayer();
     public static void main(String[] args) {
+        
         ProtoGUI newGUI = new ProtoGUI();
         newGUI.setVisible(true);
     }
